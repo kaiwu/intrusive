9ac4ba4db40e6786ba6d5f89e98a699f23b016bc
kaiwu <kaiwu2004@gmail.com>
2021-11-20 09:11:34 +0800

    cmake tests

--- test/catch_main.cpp
+++ test/catch_main.cpp
@@ -1,3 +1,11 @@
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+TEST_CASE("intrusive queue", "[]")
+{
+}
+
+TEST_CASE("intrusive binary search tree", "[]")
+{
+}
---
+++ test/test_intrusive.cpp
@@ -0,0 +1,5 @@
+#include "catch.hpp"
+#include "intrusive_bst.h"
+#include "intrusive_queue.h"
+
+
