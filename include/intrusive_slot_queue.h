753ab3753975de4ab93a5e98c9d4e1e7c135b9ca
kaiwu <kaiwu2004@gmail.com>
2021-11-25 17:16:37 +0800

    another approach

--- include/intrusive_slot_queue.h
+++ include/intrusive_slot_queue.h
@@ -3,107 +3,63 @@
 #include <stdint.h>
 
 constexpr uint32_t npos = UINT32_MAX;
+typedef uint32_t SLOT_QUEUE[3];
 
-struct link {
-  uint32_t next;
-  uint32_t prev;
+#define SLOT_QUEUE_SLOT(q)       (*(uint32_t *) &((*(q))[0]))
+#define SLOT_QUEUE_NEXT(q)       (*(uint32_t *) &((*(q))[1]))
+#define SLOT_QUEUE_PREV(q)       (*(uint32_t *) &((*(q))[2]))
 
-  link() : next(npos), prev(npos) {}
-};
+#define SLOT_QUEUE_DATA(ptr, type, field)                                          \
+  ((type *) ((char *) (ptr) - offsetof(type, field)))
 
-// alloc->address(npos) -> nullptr
-template <typename T, typename Alloc>
+#define SLOT_QUEUE_ADDR(ptr, type, field)                                          \
+  ((SLOT_QUEUE *) ((char *) (ptr) + offsetof(type, field)))
+
+#define SLOT_QUEUE_INIT(q, s)                                                      \
+  do {                                                                             \
+    SLOT_QUEUE_SLOT(q) = (s);                                                      \
+    SLOT_QUEUE_NEXT(q) = (s);                                                      \
+    SLOT_QUEUE_PREV(q) = (s);                                                      \
+  }                                                                                \
+  while (0)
+
+template<typename T>
+T* address(uint32_t slot);
+
+#define SLOT_QUEUE_EMPTY(q)                                                        \
+  (SLOT_QUEUE_SLOT(q) == SLOT_QUEUE_NEXT(q))
+
+template<typename T>
 struct intrusive_slot_queue {
 private:
-  link head;
-  Alloc* alloc;
+  SLOT_QUEUE head;
 
 public:
-  intrusive_slot_queue(Alloc* a): alloc(a) {}
+  intrusive_slot_queue() {
+    SLOT_QUEUE_INIT(&head, npos);
+  }
 
   bool empty() const noexcept {
-    return head.next == npos && head.prev == npos;
   }
 
   T* front() const noexcept {
-    return empty() ? nullptr : alloc->address(head.next);
   }
 
   void enqueue_back(T* t) {
-    if (empty()) {
-        head.next = t->slot;
-        head.prev = t->slot;
-        t->link.next = npos;
-        t->link.prev = npos;
-    }
-    else {
-        T* last = alloc->address(head.prev);
-        last->link.next = t->slot;
-        t->link.prev = last->slot;
-        t->link.next = npos;
-        head.prev = t->slot;
-    }
   }
 
   void enqueue_front(T* t) {
-    if (empty()) {
-        head.next = t->slot;
-        head.prev = t->slot;
-        t->link.next = npos;
-        t->link.prev = npos;
-    }
-    else {
-        T* first = alloc->address(head.next);
-        first->link.prev = t->slot;
-        t->link.next = first->slot;
-        t->link.prev = npos;
-        head.next = t->slot;
-    }
   }
 
   void dequeue(T* t) {
-    T* p = alloc->address(t->link.prev);
-    T* n = alloc->address(t->link.next);
-
-    if (p == nullptr && n == nullptr) { // t is both head and tail
-      head.next = npos; 
-      head.prev = npos;
-    }
-    if (p == nullptr && n != nullptr) { // t is head
-      head.next = n->slot; 
-      n->link.prev = npos;
-    }
-    if (p != nullptr && n == nullptr) { // t is tail
-      head.prev = p->slot; 
-      p->link.prev = npos;
-    }
-    if (p != nullptr && n != nullptr) { // t is in the middle
-      p->link.next = n->slot;
-      n->link.prev = p->slot;
-    }
-    t->link = link();
   }
 
   template <typename F, typename... Args>
   void iterate(F&& f, Args&&... args) const noexcept {
-    T* n = alloc->address(head.next);
-    while (n != nullptr) {
-      if(!f(n, args...)) {
-        break;
-      }
-      n = alloc->address(n->link.next);
-    }
   }
 
   template <typename F, typename... Args>
   void iterate_r(F&& f, Args&&... args) const noexcept{
-    T* p = alloc->address(head.prev);
-    while (p != nullptr) {
-      if(!f(p, args...)) {
-        break;
-      }
-      p = alloc->address(p->link.prev);
-    }
   }
 
   size_t size() const noexcept {
@@ -143,16 +99,6 @@ public:
   T* insert_before(T* p, F&& f) {
     T* x = find(f);
     if (x != nullptr) {
-      T* prev = alloc->address(x->link.prev);
-      if (prev == nullptr) {
-        enqueue_front(p);
-      }
-      else {
-        prev->link.next = p->slot;
-        p->link.prev = prev->slot;
-        p->link.next = x->slot;
-        x->link.prev = p->slot;
-      }
     }
     return x;
   }
@@ -161,16 +107,6 @@ public:
   T* insert_after(T* p, F&& f) {
     T* x = find(f);
     if (x != nullptr) {
-      T* next = alloc->address(x->link.next);
-      if (next == nullptr) {
-        enqueue_back(p);
-      }
-      else {
-        next->link.prev = p->slot;
-        p->link.next = next->slot;
-        p->link.prev = x->slot;
-        x->link.next = p->slot;
-      }
     }
     return x;
   }
